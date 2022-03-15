a5270a5a3f20c62c9147ad2428755f93347c0a9a
Edgar Trejo Avila <edgar.avila.rro@gmail.com>
2022-03-15 13:24:01 -0600

    Included GPL
    ---
    I hadn't ever properly included the GPL before;
    I just put the license within the parent folder of
    my project xd, but I already read how it is supposed
    to be used and I guess this is right.
    
    Also, created a separate file called notes.txt (which
    is not software libre; but it isn't copyrighted either)
    which is just some things I found about pointers when
    I was wandering and doing some tests in C.

--- main.ino
+++ main.ino
@@ -1,3 +1,19 @@
+/**
+ * © Copyright 2022 Edgar Trejo Avila
+ * 
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. 
+ * If not, see <https://www.gnu.org/licenses/>. 
+ * 
+ * */
+
 /** 
  * Lectura que indica que el sensor no está detectando nada 
  * */
@@ -241,9 +257,7 @@ public:
 
 MotionConnection mainConnection(5, 1);
 
-void setup()
-{
-};
+void setup() {};
 
 void loop()
 {
